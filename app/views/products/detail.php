b947dd7721333942ddeff1286b09a6f3f427fc0f
Josh06 <Admin@chaucongtu.gq>
2023-05-22 16:30:39 +0700

    structure clean

--- app/views/products/detail.php
+++
@@ -1,7 +0,0 @@
-CHI TIẾT SẢN PHẨM
-<?php
-extract($detail_product);
-echo '<br>Mã sản phẩm: ' . $id;
-echo '<br>Tên sản phẩm: ' . $name;
-echo '<br>Giá sản phẩm: ' . $gia.' VNĐ';
-?>
--- app/views/products/list.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo $query;
-// foreach($query as $values){
-//     extract($values);
-//     echo '</b><br/>------------------------</br>Type ID: <b>' . $type_id;
-//     echo '</b><br/>Type name: <b>'.$name;
-//     echo '</b><br/>Type Slug: <b>' . $slug_type;
-// }
-?>
